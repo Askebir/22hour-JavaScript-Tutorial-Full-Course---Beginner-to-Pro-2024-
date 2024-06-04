e08b0d49c1ebc59efea3ec98536c229921dc8eab
Askebir <askebirtadesse@gmail.com>
2024-06-04 12:33:14 -0700

    finall commit

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -6,22 +6,31 @@ const todoList  = [{name:'make dinner',
 renderTodoList()
 
 
-todoList.forEach(function(todoObject,index ){
+todoList.forEach((todoObject,index ) =>{
  
     const {name, dueDat} = todoObject;
     const html = 
     `<div>${name}</div>
      <div>${dueDate}</div>
-    <button  onClick="
-       
-      todoList.splice(${index}, 1);
-      renderTodoList()
-    "
-   class="delete-todo-button" >Delete</button>
+    <button  
+   class="delete-todo-button js-delete-todo-button" >Delete</button>
     `;
-    todoListHTML +=html
+    todoListHTML +=html;
     
 })
+
+document.querySelector('.js-todo-list')
+.innerHTML = todoListHTML;
+
+ document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index)=>{
+    deleteButton.addEventListener('click', ()=>{
+        todoList.splice(index, 1);
+        renderTodoList()
+
+    } )
+
+
+ })
 
 function renderTodoList(){
     let todoListHTML = '';
@@ -34,6 +43,9 @@ document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
 
+document.querySelector('.js-add-todo-button').addEventListener('click', ()=>{
+    addTodo();
+})
 
 
 function addTodo(){
